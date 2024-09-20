986153b6cb06dd12896fb48aa4d36bb43a22dd98
Ashish-Gupta7 <ash300mr@gmail.com>
2024-09-20 10:31:44 +0530

    image animation with lenis and gsap done

---
+++ script.js
@@ -0,0 +1,48 @@
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.querySelectorAll(".elem").forEach((elm) => {
+  let image = elm.querySelector("img");
+  let tl = gsap.timeline();
+
+  let xTransform = gsap.utils.random(-100, 100);
+
+  tl.set(
+    image,
+    {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+    },
+    "start"
+  )
+    .to(
+      image,
+      {
+        scale: 0,
+        duration: 3,
+        ease: "none",
+        scrollTrigger: {
+          trigger: image,
+          start: "top 20%",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      "start"
+    )
+    .to(elm, {
+      xPercent: xTransform,
+      ease: "none",
+      scrollTrigger: {
+        trigger: image,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+});
